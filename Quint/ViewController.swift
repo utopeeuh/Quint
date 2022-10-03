c1c54b102ce5dbaf951f1dfceeec983a4d99dde0
Uto <daffamadeo@gmail.com>
2022-10-03 09:36:10 +0700

    Initial Commit

---
+++ Quint/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quint
+//
+//  Created by Tb. Daffa Amadeo Zhafrana on 03/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

49470140631b2ecc6eca8e77c826d901115811d0
Vendly <vendlytan@gmail.com>
2022-10-04 12:28:25 +0700

    First commit update: - Edit SceneDelegate for UIKit Programmatically - Create new Button (WIP) - Create helper folder for DismissKeyboard - Adding new assets

---
+++ Quint/Helper/DismissKeyboard.swift
@@ -0,0 +1,20 @@
+//
+//  DismissKeyboard.swift
+//  Quint
+//
+//  Created by Vendly on 04/10/22.
+//
+
+import UIKit
+
+public extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
--- Quint/ViewController.swift
+++ Quint/ViewController.swift
@@ -9,10 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+//    private let startBtn: UIButton = {
+//
+//        let button = UIButton(type: .system)
+//        button.setTitle("Get started", for: .normal)
+//        button.layer.cornerRadius = 5
+//        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+//        button.backgroundColor = UIColor(red: 255/255, green: 135/255, blue: 178/255, alpha: 1.5)
+//        button.setTitleColor(.white, for: .normal)
+//        button.tintColor = .white
+//        return button
+//
+//    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let button = UIButton(type: .system)
+        button.setTitle("Get started", for: .normal)
+        button.layer.cornerRadius = 5
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        button.backgroundColor = UIColor(red: 255/255, green: 135/255, blue: 178/255, alpha: 1.5)
+        button.setTitleColor(.white, for: .normal)
+        button.isEnabled = true
+
+        view.addSubview(button)
+        view.backgroundColor = .systemGray
+        
+//        configureUI()
     }
+    
+//    func configureUI() {
+//
+//        let stack = UIStackView(arrangedSubviews: [startBtn])
+//        stack.axis = .vertical
+//        stack.spacing = 16
+//
+//        view.addSubview(stack)
+//
+//    }
+
 
 
 }

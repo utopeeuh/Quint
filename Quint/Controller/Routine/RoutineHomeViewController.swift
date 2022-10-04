69b3be652199a594aaa2e9168adede07ab56e891
Stefanus Hermawan Sebastian <stefanushermawan2101@gmail.com>
2022-10-04 21:28:39 +0700

    update: uv level uistackview

--- Quint/Controller/Routine/RoutineHomeViewController.swift
+++ Quint/Controller/Routine/RoutineHomeViewController.swift
@@ -28,6 +28,7 @@ class RoutineHomeViewController: UIViewController, CLLocationManagerDelegate {
     private let UVLevelSuggestion: UILabel = {
         let label = UILabel()
         label.text = "Apply Sunscreen"
+        label.textAlignment = .center
         label.textColor = .red
         label.layer.borderColor = UIColor.red.cgColor
         label.layer.cornerRadius = 8
@@ -74,7 +75,7 @@ class RoutineHomeViewController: UIViewController, CLLocationManagerDelegate {
                 let weather = try await service.weather(for: location)
                 UVLevelParameter.text = String(describing: weather.currentWeather.uvIndex.category)
                 UVLevelPoint.text = String(describing: weather.currentWeather.uvIndex.value)
-                if weather.currentWeather.uvIndex.value >= 3{
+                if weather.currentWeather.uvIndex.value == 0{
                     UVLevelSuggestion.isHidden = false
                 }
             } catch {
@@ -83,55 +84,89 @@ class RoutineHomeViewController: UIViewController, CLLocationManagerDelegate {
         }
     }
     
-    var hStack = UIStackView()
+    private lazy var hStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.distribution = .fill
+        stackView.alignment = .leading
+        return stackView
+    }()
+    
+    private lazy var vStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        stackView.alignment = .center
+        return stackView
+    }()
     
     func configureComponents() {
-        hStack.axis = .horizontal
-        hStack.alignment = .fill
-        hStack.addArrangedSubview(UVLevelPoint)
-        hStack.addArrangedSubview(UVLevelParameter)
-        hStack.addArrangedSubview(UVLevelLabel)
-        hStack.addArrangedSubview(UVLevelSuggestion)
-        hStack.translatesAutoresizingMaskIntoConstraints = false
+        vStackView.addArrangedSubview(UVLevelLabel)
+        vStackView.addArrangedSubview(UVLevelParameter)
+        
+        hStackView.addArrangedSubview(UVLevelPoint)
+        hStackView.addArrangedSubview(vStackView)
+        hStackView.addArrangedSubview(UVLevelSuggestion)
     }
     
+    
+    
     func configureLayout() {
-        view.addSubview(UVLevelLabel)
-        view.addSubview(UVLevelParameter)
-        view.addSubview(UVLevelPoint)
-        view.addSubview(UVLevelSuggestion)
-        
-        
-
-        UVLevelLabel.snp.makeConstraints { make in
-            make.left.equalTo(view.safeAreaLayoutGuide).offset(70)
-            make.top.equalTo(40)
-        }
-
-        UVLevelParameter.snp.makeConstraints { make in
-            make.left.equalTo(70)
-            make.top.equalTo(UVLevelLabel).offset(20)
-        }
-
-        UVLevelPoint.snp.makeConstraints { make in
-            make.left.equalTo(20)
-            make.top.equalTo(UVLevelLabel).offset(2)
-            make.width.equalTo(40)
-            make.height.equalTo(40)
-        }
-
-        UVLevelSuggestion.snp.makeConstraints { make in
-            make.right.equalTo(-20)
-            make.top.equalTo(UVLevelLabel).offset(10)
-            make.height.equalTo(25)
-        }
-        
-//        view.addSubview(hStack)
-//        hStack.snp.makeConstraints { make in
-//            make.left.right.equalTo(view.safeAreaLayoutGuide).offset(20)
-//            make.top.equalTo(view.safeAreaLayoutGuide).offset(50)
+//        view.addSubview(UVLevelLabel)
+//        view.addSubview(UVLevelParameter)
+//        view.addSubview(UVLevelPoint)
+//        view.addSubview(UVLevelSuggestion)
+//
+//
+//
+//        UVLevelLabel.snp.makeConstraints { make in
+//            make.left.equalTo(view.safeAreaLayoutGuide).offset(70)
+//            make.top.equalTo(40)
+//        }
+//
+//        UVLevelParameter.snp.makeConstraints { make in
+//            make.left.equalTo(70)
+//            make.top.equalTo(UVLevelLabel).offset(20)
+//        }
+//
+//        UVLevelPoint.snp.makeConstraints { make in
+//            make.left.equalTo(20)
+//            make.top.equalTo(UVLevelLabel).offset(2)
+//            make.width.equalTo(40)
+//            make.height.equalTo(40)
+//        }
+//
+//        UVLevelSuggestion.snp.makeConstraints { make in
+//            make.right.equalTo(-20)
+//            make.top.equalTo(UVLevelLabel).offset(10)
+//            make.height.equalTo(25)
 //        }
         
+        view.addSubview(hStackView)
+        hStackView.snp.makeConstraints { make in
+            make.left.equalTo(view.safeAreaLayoutGuide).offset(20)
+            make.right.equalTo(view.safeAreaLayoutGuide).offset(-20)
+            make.top.equalTo(view.safeAreaInsets).offset(40)
+        }
+        UVLevelPoint.snp.makeConstraints { make in
+            make.width.height.equalTo(40)
+            make.top.equalToSuperview()
+        }
+        UVLevelSuggestion.snp.makeConstraints { make in
+            make.height.equalTo(25)
+            make.width.equalTo(140)
+            make.top.equalToSuperview().offset(7.5)
+        }
+        
+        UVLevelLabel.snp.makeConstraints { make in
+            make.leftMargin.equalTo(30)
+        }
+        
+        UVLevelParameter.snp.makeConstraints { make in
+            make.leftMargin.equalTo(30)
+        }
 
     }
     
@@ -145,15 +180,14 @@ class RoutineHomeViewController: UIViewController, CLLocationManagerDelegate {
     
     
     func ConfigureUI() {
-        
-        let superview = self.view
         let navbar = UIView()
-        
-        superview?.addSubview(navbar)
         navbar.backgroundColor = .white
+        self.view.addSubview(navbar)
         navbar.snp.makeConstraints { (make) -> Void in
-            make.height.equalTo(90)
-            make.width.equalTo(superview!)
+            make.left.equalTo(self.view)
+            make.right.equalTo(self.view)
+            make.top.equalTo(self.topLayoutGuide.snp.top)
+            make.bottom.equalTo(self.topLayoutGuide.snp.bottom)
         }
         configureComponents()
         configureLayout()

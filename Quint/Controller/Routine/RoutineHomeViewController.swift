33c016bf35c58802fe35560fc75b59f75318712f
Stefanus Hermawan Sebastian <stefanushermawan2101@gmail.com>
2022-10-05 10:36:28 +0700

    update: feat reminder routine

--- Quint/Controller/Routine/RoutineHomeViewController.swift
+++ Quint/Controller/Routine/RoutineHomeViewController.swift
@@ -8,6 +8,7 @@ import CoreLocation
 import UIKit
 import WeatherKit
 
+
 @available(iOS 16.0, *)
 class RoutineHomeViewController: UIViewController, CLLocationManagerDelegate {
     
@@ -33,7 +34,7 @@ class RoutineHomeViewController: UIViewController, CLLocationManagerDelegate {
         label.layer.borderColor = UIColor.red.cgColor
         label.layer.cornerRadius = 8
         label.layer.borderWidth = 1.0
-        label.isHidden = true
+//        label.isHidden = true
         return label
     }()
 
@@ -47,14 +48,14 @@ class RoutineHomeViewController: UIViewController, CLLocationManagerDelegate {
 
     private var UVLevelParameter: UILabel = {
         let label = UILabel()
-        label.text = ""
+        label.text = "MODERATE"
         label.textColor = .black
         return label
     }()
 
     private var UVLevelPoint: UILabel = {
         let label = UILabel()
-        label.text = ""
+        label.text = "4"
         label.textColor = .black
         label.textAlignment = .center
         label.layer.borderColor = UIColor.red.cgColor
@@ -75,7 +76,7 @@ class RoutineHomeViewController: UIViewController, CLLocationManagerDelegate {
                 let weather = try await service.weather(for: location)
                 UVLevelParameter.text = String(describing: weather.currentWeather.uvIndex.category)
                 UVLevelPoint.text = String(describing: weather.currentWeather.uvIndex.value)
-                if weather.currentWeather.uvIndex.value == 0{
+                if weather.currentWeather.uvIndex.value >= 3{
                     UVLevelSuggestion.isHidden = false
                 }
             } catch {
@@ -84,7 +85,7 @@ class RoutineHomeViewController: UIViewController, CLLocationManagerDelegate {
         }
     }
     
-    private lazy var hStackView: UIStackView = {
+    private lazy var hStackViewUV: UIStackView = {
         let stackView = UIStackView()
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .horizontal
@@ -93,7 +94,7 @@ class RoutineHomeViewController: UIViewController, CLLocationManagerDelegate {
         return stackView
     }()
     
-    private lazy var vStackView: UIStackView = {
+    private lazy var vStackViewUV: UIStackView = {
         let stackView = UIStackView()
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
@@ -102,58 +103,91 @@ class RoutineHomeViewController: UIViewController, CLLocationManagerDelegate {
         return stackView
     }()
     
+    private lazy var mainStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.spacing = 20
+        return stackView
+    }()
+    
+    private let reminderIconView: UIImageView = {
+        let imageView = UIImageView()
+        let targetSize = CGSize(width: 30, height: 30)
+        var image = UIImage(systemName: "exclamationmark.circle.fill")?.withTintColor(.red)
+        let scaledImage = image?.scalePreservingAspectRatio(
+            targetSize: targetSize
+        )
+        imageView.image = scaledImage
+        return imageView
+    }()
+    
+    private var reminderRoutine: UILabel = {
+        let label = UILabel()
+        label.text = "Unlock today skin condition log by finishing at least one routine"
+        label.numberOfLines = 0
+        label.textColor = .black
+        return label
+    }()
+    
+    private lazy var hStackReminder: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.distribution = .fill
+        stackView.alignment = .leading
+        stackView.spacing = 10
+        stackView.layer.borderColor = CGColor(red: 123/255, green: 123/255, blue: 123/255, alpha: 1)
+        stackView.layer.borderWidth = 1.0
+        stackView.layer.cornerRadius = 8.0
+        return stackView
+    }()
+    
+    private var todayRoutine: UILabel = {
+        let label = UILabel()
+        label.text = "Today's routines"
+        label.numberOfLines = 0
+        label.textColor = .black
+        return label
+    }()
+    
+    private var routineTableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
     func configureComponents() {
-        vStackView.addArrangedSubview(UVLevelLabel)
-        vStackView.addArrangedSubview(UVLevelParameter)
+        vStackViewUV.addArrangedSubview(UVLevelLabel)
+        vStackViewUV.addArrangedSubview(UVLevelParameter)
         
-        hStackView.addArrangedSubview(UVLevelPoint)
-        hStackView.addArrangedSubview(vStackView)
-        hStackView.addArrangedSubview(UVLevelSuggestion)
+        hStackViewUV.addArrangedSubview(UVLevelPoint)
+        hStackViewUV.addArrangedSubview(vStackViewUV)
+        hStackViewUV.addArrangedSubview(UVLevelSuggestion)
+        
+        hStackReminder.addArrangedSubview(reminderIconView)
+        hStackReminder.addArrangedSubview(reminderRoutine)
+        
+        mainStackView.addArrangedSubview(hStackViewUV)
+        mainStackView.addArrangedSubview(hStackReminder)
+        mainStackView.addArrangedSubview(todayRoutine)
     }
     
-    
-    
     func configureLayout() {
-//        view.addSubview(UVLevelLabel)
-//        view.addSubview(UVLevelParameter)
-//        view.addSubview(UVLevelPoint)
-//        view.addSubview(UVLevelSuggestion)
-//
-//
-//
-//        UVLevelLabel.snp.makeConstraints { make in
-//            make.left.equalTo(view.safeAreaLayoutGuide).offset(70)
-//            make.top.equalTo(40)
-//        }
-//
-//        UVLevelParameter.snp.makeConstraints { make in
-//            make.left.equalTo(70)
-//            make.top.equalTo(UVLevelLabel).offset(20)
-//        }
-//
-//        UVLevelPoint.snp.makeConstraints { make in
-//            make.left.equalTo(20)
-//            make.top.equalTo(UVLevelLabel).offset(2)
-//            make.width.equalTo(40)
-//            make.height.equalTo(40)
-//        }
-//
-//        UVLevelSuggestion.snp.makeConstraints { make in
-//            make.right.equalTo(-20)
-//            make.top.equalTo(UVLevelLabel).offset(10)
-//            make.height.equalTo(25)
-//        }
+        view.addSubview(mainStackView)
         
-        view.addSubview(hStackView)
-        hStackView.snp.makeConstraints { make in
+        hStackViewUV.snp.makeConstraints { make in
             make.left.equalTo(view.safeAreaLayoutGuide).offset(20)
             make.right.equalTo(view.safeAreaLayoutGuide).offset(-20)
             make.top.equalTo(view.safeAreaInsets).offset(40)
         }
+        
         UVLevelPoint.snp.makeConstraints { make in
             make.width.height.equalTo(40)
             make.top.equalToSuperview()
         }
+        
         UVLevelSuggestion.snp.makeConstraints { make in
             make.height.equalTo(25)
             make.width.equalTo(140)
@@ -167,7 +201,22 @@ class RoutineHomeViewController: UIViewController, CLLocationManagerDelegate {
         UVLevelParameter.snp.makeConstraints { make in
             make.leftMargin.equalTo(30)
         }
-
+        
+        reminderIconView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaInsets).offset(5)
+            make.left.equalTo(view.safeAreaInsets).offset(5)
+        }
+        
+        reminderRoutine.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaInsets)
+        }
+        
+        todayRoutine.snp.makeConstraints { make in
+            make.top.equalTo(hStackReminder.snp.bottom).offset(25)
+        }
+        
+        
+        
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]){
@@ -195,4 +244,35 @@ class RoutineHomeViewController: UIViewController, CLLocationManagerDelegate {
     }
     
 
+}
+
+
+extension UIImage {
+    func scalePreservingAspectRatio(targetSize: CGSize) -> UIImage {
+        // Determine the scale factor that preserves aspect ratio
+        let widthRatio = targetSize.width / size.width
+        let heightRatio = targetSize.height / size.height
+        
+        let scaleFactor = min(widthRatio, heightRatio)
+        
+        // Compute the new image size that preserves aspect ratio
+        let scaledImageSize = CGSize(
+            width: size.width * scaleFactor,
+            height: size.height * scaleFactor
+        )
+
+        // Draw and return the resized UIImage
+        let renderer = UIGraphicsImageRenderer(
+            size: scaledImageSize
+        )
+
+        let scaledImage = renderer.image { _ in
+            self.draw(in: CGRect(
+                origin: .zero,
+                size: scaledImageSize
+            ))
+        }
+        
+        return scaledImage
+    }
 }

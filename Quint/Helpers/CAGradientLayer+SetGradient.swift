b59da258922d1d54427f137e1df6f16c63b85c1f
Uto <daffamadeo@gmail.com>
2022-10-06 10:48:24 +0700

    fix: updated pods and fixed color constants

--- Quint/Helpers/CAGradientLayer+SetGradient.swift
+++ Quint/Helpers/CAGradientLayer+SetGradient.swift
@@ -10,27 +10,27 @@ import UIKit
 
 extension CAGradientLayer {
     func greenGradient(){
-        self.colors = [K.Color.lgreen, K.Color.green]
+        self.colors = [K.Color.greenLightQuint, K.Color.greenQuint]
         self.locations = [0, 0.5]
     }
     
     func yellowGradient(){
-        self.colors = [K.Color.lyellow, K.Color.yellow]
+        self.colors = [K.Color.yellowLightQuint, K.Color.yellowQuint]
         self.locations = [0, 0.5]
     }
     
     func orangeGradient(){
-        self.colors = [K.Color.lorange, K.Color.orange]
+        self.colors = [K.Color.orangeLightQuint, K.Color.orangeQuint]
         self.locations = [0, 0.5]
     }
     
     func redGradient(){
-        self.colors = [K.Color.lred, K.Color.red]
+        self.colors = [K.Color.redLightQuint, K.Color.redQuint]
         self.locations = [0, 0.5]
     }
     
     func purpleGradient(){
-        self.colors = [K.Color.lpurple, K.Color.purple]
+        self.colors = [K.Color.purpleLightQuint, K.Color.purpleQuint]
         self.locations = [0, 0.5]
     }
 }

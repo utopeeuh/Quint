f3d32a2abd99897814772e9a5ca17ca0ded9ccf3
Stefanus Hermawan Sebastian <stefanushermawan2101@gmail.com>
2022-10-10 10:36:53 +0700

    update: custom backbutton Morning Routine View Controller

--- Quint/Routines/Controller/Routine/MorningRoutinesViewController.swift
+++ Quint/Routines/Controller/Routine/MorningRoutinesViewController.swift
@@ -7,23 +7,34 @@
 
 import UIKit
 
+@available(iOS 16.0, *)
 class MorningRoutinesViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemMint
-        // Do any additional setup after loading the view.
+        view.backgroundColor =  UIColor(red: 244/255, green: 244/255, blue: 244/255, alpha: 1)
+        navBar()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func navBar() {
+        let button = UIButton(type: .custom)
+            //Set the image
+        button.setImage(UIImage(systemName: "chevron.backward"), for: .normal)
+            //Set the title
+        button.setTitle(" ", for: .normal)
+        //Add target
+        button.addTarget(self, action: #selector(goToHomeRoutine), for: .touchUpInside)
+        button.tintColor = UIColor(red: 125/255, green: 125/255, blue: 125/255, alpha: 1)
+        button.frame = CGRect(x: 0, y: 0, width: 100, height: 30)
+        button.sizeToFit()
+        let barButton = UIBarButtonItem(customView: button)
+        navigationItem.leftBarButtonItem = barButton
     }
-    */
+    
+    @objc func goToHomeRoutine() {
+        let controller = RoutineHomeViewController()
+        navigationController?.popViewController(animated: true)
+    }
+
 
 }

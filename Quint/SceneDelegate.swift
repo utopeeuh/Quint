3e59d4c4ce9c0dbeeed5a9c1cdcf691f75411617
Vendly <vendlytan@gmail.com>
2022-10-24 12:48:23 +0700

    update: push to new branch

--- Quint/SceneDelegate.swift
+++ Quint/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
-        let nav = UINavigationController(rootViewController: RoutineHomeViewController())
+        let nav = UINavigationController(rootViewController: LoginVC())
         
         window?.rootViewController = nav
         window?.makeKeyAndVisible()

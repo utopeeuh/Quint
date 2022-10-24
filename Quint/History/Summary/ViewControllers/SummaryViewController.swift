ec8a0106b7c95d339e66bad1185223bb31932773
Stefanus Hermawan Sebastian <stefanushermawan2101@gmail.com>
2022-10-24 13:21:54 +0700

    update: new branch for feat history-summary

---
+++ Quint/History/Summary/ViewControllers/SummaryViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SummaryViewController.swift
+//  Quint
+//
+//  Created by Stefanus Hermawan Sebastian on 24/10/22.
+//
+
+import UIKit
+
+class SummaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    override func configureComponents() {
+        
+    }
+    
+    override func configureLayout() {
+        
+    }
+
+}

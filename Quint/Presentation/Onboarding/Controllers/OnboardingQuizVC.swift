36939543293bbd4cbcf320af31200d81cc923f55
Uto <daffamadeo@gmail.com>
2022-11-19 23:07:57 +0700

    fix: skin type id out of bounds

--- Quint/Presentation/Onboarding/Controllers/OnboardingQuizVC.swift
+++ Quint/Presentation/Onboarding/Controllers/OnboardingQuizVC.swift
@@ -139,7 +139,7 @@ class OnboardingQuizVC: UIViewController, PhotoConfirmationVCDelegate {
         
         switch(childContents[currIndex]){
         case is SkinTypeView:
-            data.selectedSkinType = (childContents[currIndex] as! SkinTypeView).getSkinType()
+            data.selectedSkinType = (childContents[currIndex] as! SkinTypeView).getSkinType()+1
             print(data.selectedSkinType!)
             break
         case is SkinConditionView:
